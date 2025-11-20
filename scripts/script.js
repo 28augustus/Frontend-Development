3f9f975cc97b013004393a63fc05afdacd35d2a8
28augustus <jadasedoc@gmail.com>
2025-11-20 23:13:55 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
 // JavaScript Document
 console.log("hi");
+
+const deMenuButton = document.querySelector("button");
+
+deMenuButton.onclick = toggleMenu;
+
+function toggleMenu() {
+  deMenuButton.classList.toggle("is-open");
+}
